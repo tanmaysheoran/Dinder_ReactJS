d2c91d12c6b2ba0767286d164a3eb0d026b58216
Tanmay Sheoran <39663470+tanmaysheoran@users.noreply.github.com>
2021-01-21 17:35:23 +0530

    Add files via upload
    adding src files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DogTest from "./components/DogTest"
+function App() {
+  return (
+    <div className="App">
+      <DogTest/>
+    </div>
+  );
+}
+
+export default App;
